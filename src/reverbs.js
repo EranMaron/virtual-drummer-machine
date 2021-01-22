97ea9d4c9aa4e3d82b17f0fa4ceeac6c55e406ce
Eran Maron <eran9maron@gmail.com>
2021-01-22 13:04:05 +0200

    Added master reverb effect

---
+++ src/reverbs.js
@@ -0,0 +1,9 @@
+const reverbs = {
+  None: "",
+  "French 18th Century Salon": require("./assets/impulse/French 18th Century Salon.wav"),
+  "Greek 7 Echo Hall": require("./assets/impulse/Greek 7 Echo Hall.wav"),
+  "Nice Drum Room": require("./assets/impulse/Nice Drum Room.wav"),
+  "Small Drum Room": require("./assets/impulse/Small Drum Room.wav"),
+};
+
+export default reverbs;
